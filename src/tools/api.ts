e122177fd032199e54b1b7f166e52f3635338b7b
EVILSTUPID <70096379+EVILSTUPID@users.noreply.github.com>
2022-02-16 16:09:21 +0300

    1
    1

--- src/tools/api.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://api.themoviedb.org/3/'
-export const API_KEY = `${process.env.REACT_APP_MY_APP_KEY}`
-export const API_IMAGE_URL = `https://image.tmdb.org/t/p/original`
